f1711685626f04df6c946ac46467217d2539035a
James Jarvis <git@jamesjarvis.io>
2019-10-13 23:59:03 +0100

    rewrote api to use a different cache - couldnt figure out how to prevent badger race conditions

--- pkg/api/handlers.go
+++ pkg/api/handlers.go
@@ -5,10 +5,16 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
-
-	mux "github.com/julienschmidt/httprouter"
 )
+
+// HandleError simply logs and exits the program if the error exists
+func HandleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 //ErrorJSON is the response sent back in case of an error
 type ErrorJSON struct {
@@ -16,7 +22,7 @@ type ErrorJSON struct {
 }
 
 //Query performs a read only query
-func Query(w http.ResponseWriter, r *http.Request, _ mux.Params) {
+func Query(w http.ResponseWriter, r *http.Request) {
 
 	// Read request body and close it
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
@@ -29,6 +35,29 @@ func Query(w http.ResponseWriter, r *http.Request, _ mux.Params) {
 	if err != nil {
 		w.WriteHeader(500)
 		errorJSON := ErrorJSON{Status: "Query Failed", Error: "Query not correctly formatted."}
+		marshalled, marshallErr := json.Marshal(errorJSON)
+		HandleError(marshallErr)
+		fmt.Fprintf(w, string(marshalled))
+		HandleError(err)
+	} else {
+		fmt.Fprintf(w, *result)
+	}
+}
+
+//GetQuery performs a read only query without badger
+func GetQuery(w http.ResponseWriter, r *http.Request) {
+	// Read request body and close it
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	HandleError(err)
+	defer r.Body.Close()
+
+	//Retrieve query result
+	result, err := PerformQuery(string(body))
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		HandleError(err)
+		w.WriteHeader(500)
+		errorJSON := ErrorJSON{Status: "Query Failed", Error: "Query probably not correctly formatted."}
 		marshalled, marshallErr := json.Marshal(errorJSON)
 		HandleError(marshallErr)
 		fmt.Fprintf(w, string(marshalled))
